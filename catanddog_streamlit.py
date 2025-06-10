4b2b44c279b7c5ddc29cebd7fa8751cbf94952cb
Dr. Tony Anyanwu <anthonynzubechukwu5448@gmail.com>
2025-06-10 15:22:30 +0100

    Update catanddog_streamlit.py

--- catanddog_streamlit.py
+++ catanddog_streamlit.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-model = keras.models.load_model('CatandDog.h5')
+model = tf.keras.models.load_model('CatandDog.h5')
 
 st.title('Cat and Dog Classifier')
 
@@ -11,12 +11,12 @@ upload = st.file_uploader('Upload an image', type=['jpg', 'png'])
 
 img_size = 100
 
-if upload():
+if upload:
     image = Image.open(upload)
     st.image(image, caption='Uploaded image')
 
     image = image.resize((img_size, img_size))
-    image = image.array(image)/255
+    image = np.array(image)/255.0
     image = np.expand_dims(image, axis = 0)
 
 
